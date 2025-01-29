d7c588fb5a395109605d61cccaf8d9c0d55ef365
SamuelOshin <samuelt.oshin@gmail.com>
2025-01-29 10:23:43 +0100

    add requirements.txt and update GitHub URL in views.py

--- api/views.py
+++ api/views.py
@@ -6,6 +6,6 @@ def hng_api(request):
     response_data = {
         "email": "samuelt.oshin@gmail.com", 
         "current_datetime": datetime.now(pytz.utc).isoformat(),
-        "github_url": "https://github.com/yourusername/your-repo", 
+        "github_url": "https://github.com/SamuelOshin/hngapitask0", 
     }
     return JsonResponse(response_data)
